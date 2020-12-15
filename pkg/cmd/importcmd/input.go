b3f098232b34e7759cd775a42d5a673be88c0477
James Strachan <james.strachan@gmail.com>
2020-12-15 06:54:34 +0000

    fix: lets use a warning message the error
    as go linters don't like punctuation in error messages

--- pkg/cmd/importcmd/input.go
+++ pkg/cmd/importcmd/input.go
@@ -75,7 +75,8 @@ func (o *ImportOptions) getOwners(userName string) ([]string, error) {
 		Size: 500,
 	})
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to list git organisations for user %s. Please update '$HOME/git/credentials' file with format 'https://<Username>:<Personal Access Token>@github.com'", userName)
+		log.Logger().Warn("Please make sure that the file '$HOME/git/credentials' containes a valid API token in the format 'https://<Username>:<Personal Access Token>@github.com'")
+		return nil, errors.Wrapf(err, "failed to list git organisations for user %s", userName)
 	}
 	for _, org := range orgs {
 		names = append(names, org.Name)
