3f43da4ba48469c4825cd8aca489c9cd483a598e
James Strachan <james.strachan@gmail.com>
2021-01-26 17:33:26 +0000

    fix: avoid creating Kptfile if a pipeline has uses:
    if we are using uses: notation from lighthouse then we don't use Kptfile to keep them in sync since we inherit everything we need from the git using simple composition + override

--- go.mod
+++ go.mod
@@ -10,13 +10,15 @@ require (
 	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
 	github.com/denormal/go-gitignore v0.0.0-20180713143441-75ce8f3e513c
 	github.com/google/uuid v1.1.4
+	github.com/googleapis/gnostic v0.5.3 // indirect
 	github.com/jbrukh/bayesian v0.0.0-20200318221351-d726b684ca4a // indirect
-	github.com/jenkins-x/go-scm v1.5.212
+	github.com/jenkins-x/go-scm v1.5.216
 	github.com/jenkins-x/jx-api/v4 v4.0.23
-	github.com/jenkins-x/jx-gitops v0.0.530
-	github.com/jenkins-x/jx-helpers/v3 v3.0.64
+	github.com/jenkins-x/jx-gitops v0.0.531
+	github.com/jenkins-x/jx-helpers/v3 v3.0.71
 	github.com/jenkins-x/jx-logging/v3 v3.0.3
-	github.com/jenkins-x/jx-promote v0.0.176
+	github.com/jenkins-x/jx-promote v0.0.179
+	github.com/jenkins-x/lighthouse-client v0.0.0-20210118141307-27a29c02a663
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
--- pkg/cmd/importcmd/add_missing_kptfiles.go
+++ pkg/cmd/importcmd/add_missing_kptfiles.go
@@ -2,9 +2,15 @@ package importcmd
 
 import (
 	"fmt"
+	"github.com/jenkins-x/jx-helpers/v3/pkg/yamls"
+	"github.com/jenkins-x/lighthouse-client/pkg/config/job"
+	"github.com/jenkins-x/lighthouse-client/pkg/triggerconfig"
+	"github.com/jenkins-x/lighthouse-client/pkg/triggerconfig/inrepo"
+	tektonv1beta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sigs.k8s.io/yaml"
 	"strings"
 
 	"github.com/jenkins-x/jx-helpers/v3/pkg/files"
@@ -43,12 +49,12 @@ func (o *ImportOptions) createMissingLighthouseKptFiles(lighthouseDir, packName 
 		}
 		name := f.Name()
 
-		triggerFile := filepath.Join(lighthouseDir, name, "triggers.yaml")
+		dir := filepath.Join(lighthouseDir, name)
+		triggerFile := filepath.Join(dir, "triggers.yaml")
 		exists, err := files.FileExists(triggerFile)
 		if err != nil {
 			return errors.Wrapf(err, "failed to check if file exists %s", triggerFile)
 		}
-
 		if !exists {
 			continue
 		}
@@ -60,39 +66,188 @@ func (o *ImportOptions) createMissingLighthouseKptFiles(lighthouseDir, packName 
 		if err != nil {
 			return errors.Wrapf(err, "failed to check if file exists %s", localKptFile)
 		}
-		if !exists {
-			sha, err := gitclient.GetLatestCommitSha(o.Git(), lighthouseDir)
-			if err != nil {
-				return errors.Wrapf(err, "failed to discover latest git commit for dir %s", lighthouseDir)
-			}
-
-			gitURL, err := gitdiscovery.FindGitURLFromDir(lighthouseDir)
-			if err != nil {
-				return errors.Wrapf(err, "failed to discover git URL in dir %s", lighthouseDir)
-			}
-
-			// lets remove any user/passwords just in case
-			gitURL = stringhelpers.SanitizeURL(gitURL)
-
-			if gitURL == "" {
-				return errors.Errorf("failed to find git URL in dir %s", lighthouseDir)
-			}
-
-			err = os.MkdirAll(localKptDir, files.DefaultDirWritePermissions)
-			if err != nil {
-				return errors.Wrapf(err, "failed to create dir %s", localKptDir)
-			}
-
-			fromDir := filepath.Join("/packs", packName, ".lighthouse", name)
-			gitURL = strings.TrimSuffix(gitURL, ".git")
-			text := fmt.Sprintf(kptFile, name, sha, gitURL, fromDir)
-			err = ioutil.WriteFile(localKptFile, []byte(text), files.DefaultFileWritePermissions)
-			if err != nil {
-				return errors.Wrapf(err, "failed to save file %s", localKptFile)
-			}
-
-			log.Logger().Infof("created %s", localKptFile)
+		if exists {
+			continue
 		}
+
+		hasUses, err := CheckForUsesImage(dir, triggerFile)
+		if err != nil {
+			return errors.Wrapf(err, "failed to check for image: uses:sourceURI")
+		}
+		if hasUses {
+			continue
+		}
+
+		sha, err := gitclient.GetLatestCommitSha(o.Git(), lighthouseDir)
+		if err != nil {
+			return errors.Wrapf(err, "failed to discover latest git commit for dir %s", lighthouseDir)
+		}
+
+		gitURL, err := gitdiscovery.FindGitURLFromDir(lighthouseDir)
+		if err != nil {
+			return errors.Wrapf(err, "failed to discover git URL in dir %s", lighthouseDir)
+		}
+
+		// lets remove any user/passwords just in case
+		gitURL = stringhelpers.SanitizeURL(gitURL)
+
+		if gitURL == "" {
+			return errors.Errorf("failed to find git URL in dir %s", lighthouseDir)
+		}
+
+		err = os.MkdirAll(localKptDir, files.DefaultDirWritePermissions)
+		if err != nil {
+			return errors.Wrapf(err, "failed to create dir %s", localKptDir)
+		}
+
+		fromDir := filepath.Join("/packs", packName, ".lighthouse", name)
+		gitURL = strings.TrimSuffix(gitURL, ".git")
+		text := fmt.Sprintf(kptFile, name, sha, gitURL, fromDir)
+		err = ioutil.WriteFile(localKptFile, []byte(text), files.DefaultFileWritePermissions)
+		if err != nil {
+			return errors.Wrapf(err, "failed to save file %s", localKptFile)
+		}
+
+		log.Logger().Infof("created %s", localKptFile)
 	}
 	return nil
+}
+
+// CheckForUsesImage checks if the given dir and trigger file has a uses: image
+// which if present assumes we are using uses: inheritance rather than kpt
+func CheckForUsesImage(dir, triggersFile string) (bool, error) {
+	repoConfig := &triggerconfig.Config{}
+	err := yamls.LoadFile(triggersFile, repoConfig)
+	if err != nil {
+		return false, errors.Wrapf(err, "failed to load lighthouse triggers: %s", triggersFile)
+	}
+
+	for i := range repoConfig.Spec.Presubmits {
+		r := &repoConfig.Spec.Presubmits[i]
+		if r.SourcePath != "" {
+			path := filepath.Join(dir, r.SourcePath)
+			flag, err := loadJobBaseFromSourcePath(path)
+			if err != nil {
+				log.Logger().Warnf("failed to load file %s", path)
+			}
+			if flag {
+				return true, nil
+			}
+		}
+		if r.Agent == "" && r.PipelineRunSpec != nil {
+			r.Agent = job.TektonPipelineAgent
+		}
+	}
+	for i := range repoConfig.Spec.Postsubmits {
+		r := &repoConfig.Spec.Postsubmits[i]
+		if r.SourcePath != "" {
+			path := filepath.Join(dir, r.SourcePath)
+			flag, err := loadJobBaseFromSourcePath(path)
+			if err != nil {
+				log.Logger().Warnf("failed to load file %s", path)
+			}
+			if flag {
+				return true, nil
+			}
+		}
+	}
+	return false, nil
+}
+
+func loadJobBaseFromSourcePath(path string) (bool, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return false, errors.Wrapf(err, "failed to load file %s", path)
+	}
+	if len(data) == 0 {
+		return false, errors.Errorf("empty file file %s", path)
+	}
+
+	pr, err := LoadTektonResourceAsPipelineRun(data, "for file "+path)
+	if err != nil {
+		return false, errors.Wrapf(err, "failed to unmarshal YAML file %s", path)
+	}
+	ps := pr.Spec.PipelineSpec
+	if ps == nil {
+		return false, nil
+	}
+	for i := range ps.Tasks {
+		task := &ps.Tasks[i]
+		if task.TaskSpec == nil {
+			continue
+		}
+		ts := task.TaskSpec.TaskSpec
+		if ts.StepTemplate != nil && isUsesImage(ts.StepTemplate.Image) {
+			return true, nil
+		}
+		steps := ts.Steps
+		for _, step := range steps {
+			if isUsesImage(step.Image) {
+				return true, nil
+			}
+		}
+	}
+	return false, nil
+}
+
+func isUsesImage(image string) bool {
+	return strings.HasPrefix(image, "uses:")
+}
+
+// LoadTektonResourceAsPipelineRun loads a PipelineRun, Pipeline, Task or TaskRun and convert it to a PipelineRun
+func LoadTektonResourceAsPipelineRun(data []byte, message string) (*tektonv1beta1.PipelineRun, error) {
+	defaultValues, err := inrepo.NewDefaultValues()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to parse default values")
+	}
+
+	kindPrefix := "kind:"
+	kind := "PipelineRun"
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		if !strings.HasPrefix(line, kindPrefix) {
+			continue
+		}
+		k := strings.TrimSpace(line[len(kindPrefix):])
+		if k != "" {
+			kind = k
+			break
+		}
+	}
+	switch kind {
+	case "Pipeline":
+		pipeline := &tektonv1beta1.Pipeline{}
+		err := yaml.Unmarshal(data, pipeline)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to unmarshal Pipeline YAML %s", message)
+		}
+		return inrepo.ConvertPipelineToPipelineRun(pipeline, message, defaultValues)
+
+	case "PipelineRun":
+		prs := &tektonv1beta1.PipelineRun{}
+		err := yaml.Unmarshal(data, prs)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to unmarshal PipelineRun YAML %s", message)
+		}
+		return prs, nil
+
+	case "Task":
+		task := &tektonv1beta1.Task{}
+		err := yaml.Unmarshal(data, task)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to unmarshal Task YAML %s", message)
+		}
+		return inrepo.ConvertTaskToPipelineRun(task, message, defaultValues)
+
+	case "TaskRun":
+		tr := &tektonv1beta1.TaskRun{}
+		err := yaml.Unmarshal(data, tr)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to unmarshal TaskRun YAML %s", message)
+		}
+		return inrepo.ConvertTaskRunToPipelineRun(tr, message, defaultValues)
+
+	default:
+		return nil, errors.Errorf("kind %s is not supported for %s", kind, message)
+	}
 }
